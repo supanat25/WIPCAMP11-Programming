dbe15c1c8eb14f2bad5ef54a7f389c306d6c3b0b
supanat25 <supanat1012@gmail.com>
2019-05-30 11:37:28 +0700

    [ADD] Newproject
    first time java

---
+++ WIP-XI/src/III/Output/Print.java
@@ -0,0 +1,23 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("ไตเติ้ล");
+        
+        System.out.println("มัจฉะ");
+        
+        // \n <-- new line (ชี้นบรรทัดใหม่)
+        System.out.println("อั๋น");
+        
+        System.out.println("บุค");
+        
+    }
+}
